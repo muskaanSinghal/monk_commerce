62c80490b6ee2401c7c8273608cd318bfd8b6ab7
muskaan_singhal <muskaan@ascendeum.com>
2026-02-10 15:58:26 +0530

    Added main component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import ProductList from './components/ProductList';
+import logo from "./assets/icons/icon-logo.svg";
+
 import './App.css'
 
 function App() {
   return (
     <>
-      <p>CODE HERE</p>
+      {/* App Header */}
+      <div className={"header"}>
+        <img className={"logo"} src={logo} />
+        <h1>Monk Upsell & Cross-sell</h1>
+      </div>
+      {/* Main Component */}
+      <ProductList />
     </>
   )
 }
---
+++ src/components/Readme.md
@@ -0,0 +1,8 @@
+This folder contains all the components rendered in the app.
+
+I have added view and logic components together however, for bigger apps we can separate them into views and components for separating logic and design.
+
+Components are of two types in here:
+
+1. Related to product and variants
+2. Global level ui components like modals. it can be extended to add components like drawers/accordians etc.
